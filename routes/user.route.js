b4f627fabb87ae100eeca3b5f0ce01f61d17e31c
shishanee <mansur72095@gmail.com>
2023-08-03 15:44:44 +0300

    commit

--- routes/user.route.js
+++ routes/user.route.js
@@ -8,6 +8,7 @@ const router = Router();
 router.post("/auth", userController.registerUser); // Роут регистрации пользователя
 router.post("/login", userController.login); // Вход в учетную запись
 router.get("/user", authMiddleware, userController.getUser); // Вывод пользователя
+router.get('/users', userController.allUsers) // все пользователи
 router.patch('/addfollow', authMiddleware, userController.addFollow) // Подписаться
 router.get('/allfollow', authMiddleware, userController.allFollow) // Все подписки
 
