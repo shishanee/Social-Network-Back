48f34068aa3299986f1c052174de61862af6f1fb
shishanee <mansur72095@gmail.com>
2023-08-04 17:59:45 +0300

    code review

--- routes/dialog.route.js
+++ routes/dialog.route.js
@@ -5,8 +5,8 @@ const authMiddleware = require("../middlewares/auth.middleware");
 const router = Router();
 
 router.post("/createDialog", authMiddleware, dialogController.createDialog); // создание чата
-router.patch('/addmessage', authMiddleware, dialogController.addMessage) // отправка сообщения
-router.get('/getDialogs', dialogController.getDialogs) // вывод всех диалогов
-
+router.get("/oneDialog/:id", dialogController.oneDialog); // создание чата
+router.patch("/addmessage/:id", authMiddleware, dialogController.addMessage); // отправка сообщения
+router.get("/getDialogs", dialogController.getDialogs); // вывод всех диалогов
 
 module.exports = router;
