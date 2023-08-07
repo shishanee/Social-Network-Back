58ed9ae22de0d2f96878bebd10e8ea73597e2ad5
yakhya1 <gaitukaev777@mail.ru>
2023-08-07 16:26:20 +0300

    commit

--- controllers/group.controller.js
+++ controllers/group.controller.js
@@ -2,9 +2,10 @@ const Group = require("../models/Group.model");
 
 module.exports.groupController = {
   createGroup: async (req, res) => {
-    const { name, discription, followers, posts } = req.body;
+    const { name, discription, followers, posts, user } = req.body;
     const data = await Group.create({
       name,
+      user,
       discription,
       followers,
       posts,
@@ -15,4 +16,9 @@ module.exports.groupController = {
     const data = await Group.find();
     res.json(data);
   },
+  getOneGroup: async (req, res) => {
+    const data= await Group.findById(req.params.id);
+
+    res.json(data)
+  }
 };
--- routes/group.route.js
+++ routes/group.route.js
@@ -4,7 +4,8 @@ const authMiddleware = require("../middlewares/auth.middleware");
 
 const router = Router();
 
-router.post("/group", authMiddleware, groupController.createGroup); // создание группы
+router.post("/group",  groupController.createGroup); // создание группы
 router.get("/group", groupController.getGroup); // вывод группы
+router.get("/group/:id", groupController.getOneGroup); //вывод одной группы по id
 
 module.exports = router;
