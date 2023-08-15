bc933d71af3345085a5cd3d83a31e341063f40fd
Yunusov <abubakar.yunusov.47@mail.ru>
2023-08-15 12:25:17 +0300

    Fav

--- controllers/favorite.controller.js
+++ controllers/favorite.controller.js
@@ -4,5 +4,20 @@ module.exports.favoriteController = {
     getPosts: async (req, res) => {
      const data = await Favorite.find();
      res.json(data);
+    },
+    addPostInFavorite : async (req, res)=> {
+        const posts = await Favorite.create({
+            post: req.body.post
+        }) 
+        res.json(posts)
+    },
+    deletePostInFavorite : async ( req, res) =>{
+        try {
+            await Favorite.findByIdAndDelete(req.body.post)
+            res.json('Удалено')
+        } catch (error) {
+            res.json('Х1ар пал хил '+ error)
+        }
     }
+
 }
--- routes/favorite.route.js
+++ routes/favorite.route.js
@@ -5,6 +5,9 @@ const { favoriteController } = require("../controllers/favorite.controller");
 const router = Router();
 
 router.get("/favorite", favoriteController.getPosts);
+router.post("/favorite", favoriteController.addPostInFavorite)
+router.delete("/favorite", favoriteController.deletePostInFavorite)
+
 
 module.exports = router;
 
